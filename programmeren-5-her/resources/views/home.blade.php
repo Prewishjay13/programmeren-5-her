b2342bd901ea42dafeff9f6d5ebff0c4b13bbe33
Prewishjay13 <45820145+Prewishjay13@users.noreply.github.com>
2023-01-07 10:44:41 +0100

    Update home.blade.php

---
+++ programmeren-5-her/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, intitial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Programmeren-5</title>
+</head>
+
+<html>
+
+<body>
+
+</body>
+</html>
